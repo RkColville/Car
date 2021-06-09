3b2be346c7ad7a5dc36879e82e4d527fc34ee509
Robert Colville <rkcolville@gmail.com>
2021-06-09 17:24:22 -0500

    updated file to reflect more information

---
+++ Car/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Car
+    {
+
+        public Car()
+        {
+
+        }
+        
+        public Car(int year, string make, string model)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+        }
+        
+        
+        //Create a Make property of type string that is public:
+        public string Make { get; set; } // get = read set = write
+
+        // Create a Model property of type string that is public:
+        public string Model { get; set; }
+
+        //Create a Year property of type int that is public:
+        public int Year {get;set;}
+
+        
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.Year = 2021;
+            myObj.Make = "Tesla";
+            myObj.Model = "Model S";
+            //Console.WriteLine(myObj.Year);
+            //Console.WriteLine(myObj.Make);
+            //Console.WriteLine(myObj.Model);
+            
+
+            var bmw = new Car()
+            {
+                Year = 2010,
+                Make = "BMW",
+                Model = "B52 LoveShack"
+                
+            };
+
+            var myCar = new Car(2003, "Toyota", "4Runner");
+
+            var carList = new List<Car>() { myCar, bmw, myObj };
+            //Console.WriteLine(carList.Capacity);
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+            }
+            Console.ReadLine();
+        }
+   }
+    
+}
